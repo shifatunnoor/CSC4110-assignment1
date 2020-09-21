2c490a342ce021b2875467816547c361eadf3dcf
Shifatunnoor <gc1425@wayne.edu>
2020-09-21 18:34:20 -0400

    Adding author name.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,7 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Header file for input output functions
+//Author : fnu shifatunnoor <gc1425@wayne.edu>
+
 #include<iostream>  
   
 using namespace std; 
